d57673e9af35165de91046066914b4f99725aed5
amoahy15 <amoakohene.humphrey15@gmail.com>
2024-02-27 10:12:06 -0500

    Implemented ability to connect to database.py must run app.py

---
+++ VirtuosoApp/backend/config.py
@@ -0,0 +1,3 @@
+# GenTech/Virtusio/backend/app/config.py
+class Config:
+    MONGODB_URI = 'mongodb+srv://hamoako:<HKoL4WKSlk638c1E>@gentech.xycfoee.mongodb.net/?retryWrites=true&w=majority&appName=GenTech'
