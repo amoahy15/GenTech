50e8a6e05aa955968d1bfda46eab7f17d9daa3d1
joiz1 <157747754+joiz1@users.noreply.github.com>
2024-02-29 15:14:34 -0500

    Updated artwork.py

--- VirtuosoApp/backend/app/models/artwork.py
+++ VirtuosoApp/backend/app/models/artwork.py
@@ -1,4 +1,4 @@
-from mongoengine import Document, StringField, IntField, DateTimeField
+from mongoengine import Document, StringField, IntField, DateTimeField, DoubleField, ListField
 from datetime import datetime
 
 # Model for Artwork
@@ -8,11 +8,14 @@ class Artwork(Document):
         artworkID = StringField(required=True, unique=True)
         title = StringField(required=True)
         artist = StringField(required=True)
-        year = IntField(required=True)
+        year = StringField(required=True)
         image_url = image_url(required=True)
+        tags = ListField(StringField(max_length=50))
         description = StringField()
         image_location = StringField()
         annotations = StringField()
+        average_rating = DoubleField()
+        
 
 meta = {
         'collection': 'Artwork'
