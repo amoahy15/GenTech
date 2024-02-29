c1014dcea542c5c451b0d5c81326a94fe0e8cd24
joiz1 <157747754+joiz1@users.noreply.github.com>
2024-02-29 01:43:18 -0500

    Update artwork.py

--- VirtuosoApp/backend/app/models/artwork.py
+++ VirtuosoApp/backend/app/models/artwork.py
@@ -1,10 +1,31 @@
-class Artwork:
-    def __init__(self, artworkID, title, artist, year, description, image_location, image_url, annotations):
-        self.artworkID = artworkID
-        self.title = title
-        self.artist = artist
-        self.year = year
-        self.description = description
-        self.image_location = image_location
-        self.image_url = image_url
-        self.annotations = annotations
+from mongoengine import Document, StringField, IntField, DateTimeField
+from datetime import datetime
+
+# Model for Artwork
+class Artwork(Document):
+    
+        # Fields
+        artworkID = StringField(required=True, unique=True)
+        title = StringField(required=True)
+        artist = StringField(required=True)
+        year = IntField(required=True)
+        image_url = image_url(required=True)
+        description = StringField()
+        image_location = StringField()
+        annotations = StringField()
+
+meta = {
+        'collection': 'Artwork'
+    }
+
+    def serialize(self):
+        return {
+            'artworkID' : self.artworkID
+            'title' : self.title
+            'artist' : self.artist
+            'year' : self.year
+            'image_url' : self.image_url
+            'description' : self.description 
+            'image_location' : self.image_location
+            'annotations' : self.annotations
+        }
