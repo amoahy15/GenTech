d3a48e1dcfbaf479b43d59cced4a4181accf5f7a
joiz1 <157747754+joiz1@users.noreply.github.com>
2024-03-06 20:58:24 -0500

    Updated artworkController.py
    Added adtional api calls

--- VirtuosoApp/backend/app/controllers/artworkController.py
+++ VirtuosoApp/backend/app/controllers/artworkController.py
@@ -32,10 +32,47 @@ def createArtwork():
     except Exception as e:
         return jsonify({'error': 'Unexpected error', 'details': str(e)}), 500
 
-    @artworkController.route('/artwork', methods=['GET'])
+@artworkController.route('/artwork', methods=['GET'])
     def artwork(artworkID):
-        artwork = Review.objects(artworkID=artworkID).first()
+        artwork = artwork.objects(artworkID=artworkID).first()
         if artwork:
             return jsonify(artwork.serialize())
         else:
             return jsonify({"error": "Artwork not found"}), 404
+
+@artworkController.route('/artwork/<artworkID>', methods=['PUT'])
+    def updateArtwork(artworkID):
+        data = request.get_json()
+
+            try:
+                artwork = Artwork.objects(artworkID=artworkID).first()
+                if not artwork:
+                    return jsonify({"error": "Artwork not found"}), 404
+
+                # Update fields if they exist in the request data
+                if 'title' in data:
+                    artwork.title = data['title']
+                if 'artist' in data:
+                    artwork.artist = data['artist']
+                if 'tags' in data:
+                    artwork.tags = data['tags']
+                if 'description' in data:
+                    artwork.description = data['description']
+                if 'image_location' in data:
+                    artwork.image_location = data['image_location']
+
+                artwork.save()
+                return jsonify({"message": "Artwork updated successfully"}), 200
+
+            except ValidationError as e:
+                return jsonify({"error": str(e)}), 400
+            except Exception as e:
+                return jsonify({'error': 'Unexpected error', 'details': str(e)}), 500
+@artworkController.route('/artwork/<artworkID>', methods=['DELETE'])
+   def delete_artwork(artworkID):
+        artwork = artwork.objects(artworkID=artworkID).first()
+         if artwork:
+                artwork.delete()
+                return jsonify({"message": "Artwork deleted successfully"})
+        else:
+                return jsonify({"error": "Artwork not found"}), 404
