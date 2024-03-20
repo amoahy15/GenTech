c637c190645ecfc83af2aa9ae996065f1e06bf84
joiz1 <157747754+joiz1@users.noreply.github.com>
2024-03-20 11:33:23 -0400

    Create annotation.py

---
+++ VirtuosoApp/backend/app/models/annotation.py
@@ -0,0 +1,9 @@
+from mongoengine import Document, StringField, DateTimeField
+
+class Annotation(Document):
+    artworkID = StringField(required=True)
+    userID = StringField(required=True)
+    message = StringField(required=True)
+    x_coordinate = StringField(required=True)
+    y_coordinate = StringField(required=True)
+    created_at = DateTimeField(required=True)DoubleField
