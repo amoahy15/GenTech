2c8e474ecc48a37e4fb893a35aac8ba3c274e628
joiz1 <157747754+joiz1@users.noreply.github.com>
2024-03-21 13:26:55 -0400

    Updated review.py

--- VirtuosoApp/backend/app/models/review.py
+++ VirtuosoApp/backend/app/models/review.py
@@ -1,13 +1,13 @@
-from mongoengine import Document, StringField, IntField, DateTimeField
+from mongoengine import Document, StringField, FloatField, DateTimeField
 from datetime import datetime
 
 
 # Model for Reviews
 class Review(Document):
     # Fields
-    reviewID = StringField(required=True, unique=True)
+    reviewID = StringField(required=True,)
     userID = StringField(required=True)
     artworkID = StringField(required=True)
-    rating = DoubleField(required =True, min_value=0.0, max_value=5.0)
+    rating = FloatField(required = True, min_value=0.0, max_value=5.0)
     comment = StringField()
     createdAt = DateTimeField(default=datetime.now)
