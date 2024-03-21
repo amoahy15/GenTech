ccad0ad72225b588817a7d3111c4152260de1daa
joiz1 <157747754+joiz1@users.noreply.github.com>
2024-03-21 13:30:10 -0400

    Update review.py

--- VirtuosoApp/backend/app/models/review.py
+++ VirtuosoApp/backend/app/models/review.py
@@ -5,7 +5,7 @@ from datetime import datetime
 # Model for Reviews
 class Review(Document):
     # Fields
-    reviewID = StringField(required=True,)
+    reviewID = StringField(required=True,unique=True)
     userID = StringField(required=True)
     artworkID = StringField(required=True)
     rating = FloatField(required = True, min_value=0.0, max_value=5.0)
