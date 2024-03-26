0623f13d38505aa9a9da986635fe29387edd6003
Ivan Martinez-Kay <martinezkayivan@gmail.com>
2024-03-26 01:32:50 -0400

    more changes to navbar

--- VirtuosoApp/frontend/src/components/Navigation/Nav.js
+++ VirtuosoApp/frontend/src/components/Navigation/Nav.js
@@ -1,6 +1,8 @@
+import React, {useState} from "react";
 import Button from "../Button"
 import styles from "../styles/main.module.css";
 import spiral from '../../assets/images/fibonacci.png';
+import {FaSearch} from "react-icons/fa";
 
 
 const Nav = () => {
@@ -8,6 +10,9 @@ const Nav = () => {
     const onClick = () => {
         console.log('click')
     }
+
+    const[input, setInput] = useState("")
+
   return (
     
     
@@ -16,7 +21,14 @@ const Nav = () => {
       
       <header className={styles.header}>
       <a className={styles.virtuoso} href = './' style = {{textDecoration: 'none'}}><span>VIRTUOS</span><span style ={{color: '#990000'}}>O</span></a>
-      <img className={styles.logo} src={spiral}/>
+      <div className={styles.searchbar}>
+        <FaSearch id = "search-icon" style={{color: 'rgb(153,0,0)'}}/>
+        <input className={styles.searchinput} 
+        placeholder="Search" 
+        value={input} 
+        onChange={(e)=>setInput(e.target.value)}/>
+      </div>
+      {/*<img className={styles.logo} src={spiral}/>*/}
       <nav className={styles.Nav}>
 
           <a href='./'><Button text='Home' onClick={onClick}/></a>
