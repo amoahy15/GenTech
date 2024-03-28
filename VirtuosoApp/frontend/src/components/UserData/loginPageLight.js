6685e46d299cc02071e607a7d36ca1d1140286e0
Ivan Martinez-Kay <martinezkayivan@gmail.com>
2024-03-28 10:38:30 -0400

    connected /login2 to the backend

--- VirtuosoApp/frontend/src/components/UserData/loginPageLight.js
+++ VirtuosoApp/frontend/src/components/UserData/loginPageLight.js
@@ -4,11 +4,39 @@ import { FaUser } from "react-icons/fa";
 import { IoLockClosedSharp } from "react-icons/io5";
 import bgVid from '../../assets/videos/lightvid.mp4';
 import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 const LoginPage2 = () => {
 
+  const [email,setEmail] = useState('');
+  const [userName, setUserName] = useState('');
+  const [password, setPassword ] = useState('');
 
+  const nav = useHistory();
+  
+  const loginUser = async (e) => {
+    e.preventDefault(); // Prevent the default form submit action
 
+    const userData = {
+      email: email,
+      password: password,
+      userName: userName
+    };
+
+    axios.post(' http://127.0.0.1:5000/api/login', userData)
+    .then(function (response) {
+      console.log(response);
+      nav.push("./profile ");
+    }).catch(function (error) {
+      console.log(error);
+      if(error.response && error.response.status === 401){
+        alert("Invalid credentials");
+      } else {
+        alert("Error. Please try again");
+      }
+    });
+  }
+      
   return (
 
     <div className={styles.main}>
@@ -22,11 +50,11 @@ const LoginPage2 = () => {
       
 
         <div className={styles.inputbox}> 
-          <input className ={styles.input} type="text" placeholder='Username' required/> 
+          <input className ={styles.input} value ={(userName)||(email)} onChange={(e) => {setUserName(e.target.value)||setEmail(e.target.value)}} type="text" placeholder='Username' required/> 
           <FaUser className={styles.icon}/>
         </div>
         <div className={styles.inputbox}> 
-          <input className ={styles.input} type="password" placeholder='Password' required/> 
+          <input className ={styles.input} value ={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder='Password' required/> 
           <IoLockClosedSharp className={styles.icon}/>
         </div>
 
@@ -35,7 +63,7 @@ const LoginPage2 = () => {
           <a className ={styles.a} href="#">Forgot password?</a>
         </div>
 
-        <button className = {styles.btn} type="submit">sign in</button>
+        <button className = {styles.btn} type="submit" onClick ={loginUser}>sign in</button>
 
         <div className={styles.register}>
           <p className={styles.p}>First time here?<a className ={styles.a2} href = './register2'> Register</a></p>
