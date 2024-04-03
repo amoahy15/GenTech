623ef5889bad82293962283fcc9e91baf4a6ebfd
amoahy15 <amoakohene.humphrey15@gmail.com>
2024-04-02 22:10:16 -0400

    Basic API works to get images from s3 bucket

--- VirtuosoApp/backend/app.py
+++ VirtuosoApp/backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from datetime import timedelta
-from flask_cors import CORS  # Ensure this is imported
+from flask_cors import CORS 
 from util.database import connect_db
 from controllers.userController import user_controller
 from controllers.reviewController import review_controller
