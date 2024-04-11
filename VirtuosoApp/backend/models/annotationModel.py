1fa95444cbefcd619f5333842a32ebe9c4130e5e
joiz1 <157747754+joiz1@users.noreply.github.com>
2024-04-11 14:54:28 -0400

    Update annotationModel.py

--- VirtuosoApp/backend/models/annotationModel.py
+++ VirtuosoApp/backend/models/annotationModel.py
@@ -2,7 +2,7 @@ from mongoengine import Document, StringField, DateTimeField
 from datetime import datetime
 
 class Annotation(Document):
-    artworkID = StringField(required=True)
+    artwork_id  = StringField(required=True)
     userID = StringField(required=True)
     message = StringField(required=True)
     x_coordinate = StringField(required=True)
