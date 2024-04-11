b22a75b3513042ad6f4b5270affe7ee505bac82a
Tierra Ablorh <122699416+tierranaa@users.noreply.github.com>
2024-04-11 14:02:19 -0400

    with hover

--- VirtuosoApp/frontend/src/components/carouselcomponents/Carousel.js
+++ VirtuosoApp/frontend/src/components/carouselcomponents/Carousel.js
@@ -1,52 +1,75 @@
-import React, { useState, useEffect } from 'react';
 import Slider from 'react-slick';
-import axios from 'axios';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import '../styles/carouselarrow.module.css';
-import ImageCard from './ImageCard';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import ImageCardHover from './ImageCardHover';
+import '../styles/carouselarrow.module.css';
 
-
-const fetchImagesFromCategory = async (category) => {
-    try {
-        const response = await axios.get(`http://127.0.0.1:5000/api/s3/images/${category}`);
-        return response.data;
-    } catch (error) {
-        console.error(`Error fetching images for category ${category}:`, error);
-        return [];
+function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, zIndex: '1', display: "block", color: "gray", width: "40px", height: "20px", textAlign: "center", lineHeight: "20px", fontSize: "36px"}}
+        onClick={onClick}
+      >
+        {'>'}
+      </div>
+    );
     }
-};
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, zIndex: '1', display: "block", color: "gray", width: "40px", height: "20px", textAlign: "center", lineHeight: "20px", fontSize: "36px"}}
+        onClick={onClick}
+      >
+        {'<'}
+      </div>
+    );
+  }
 
-const Carousel = ({ category }) => {
-  const [images, setImages] = useState([]);
-
-  useEffect(() => {
-    const fetchImages = async () => {
-      if (category) {
-        const fetchedImages = await fetchImagesFromCategory(category);
-        setImages(fetchedImages.map(url => ({ url: url, alt: `${category} image` })));
+const Carousel = ({ images }) => {
+  const settings = {
+    dots: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplaySpeed: 2000,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow/>,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        }
       }
-    };
-
-    fetchImages();
-  }, [category]);
-
-  const sliderSettings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 3,
-      slidesToScroll: 3,
+    ]
   };
 
   return (
-    <Slider {...sliderSettings}>
+    
+    <Slider {...settings}>
         {images.map((image, index) => (
-            <div key={index}>
-                <ImageCard image={image} />
-            </div>
-        ))}
+        <div key={index}>
+          <a href = './reviews'><ImageCardHover src={image} alt={`Slide ${index + 1}`} /></a>
+          
+        </div>
+      ))}
     </Slider>
   );
 };
