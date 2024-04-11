c97bd9ec80cfff6d5cf04f1ffba4be51190d7283
Ivan Martinez-Kay <martinezkayivan@gmail.com>
2024-04-11 01:40:48 -0400

    fixed issue in navbar...

--- VirtuosoApp/frontend/src/components/UserData/ProfilePic.js
+++ VirtuosoApp/frontend/src/components/UserData/ProfilePic.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import profilephoto from '../../assets/images/Frida_Kahlo/Frida_Kahlo_3.jpg';
+import styles from '../styles/profilepic.module.css'
 
 const ProfilePic = () => {
   return (
+    <div className={styles.profilephoto}>
     <img src={profilephoto} alt="Profile" />
+    </div>
   )
 }
 
