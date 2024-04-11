f253abec9e41ab97b26683e17cd8b8989db1ab5e
Tierra Ablorh <122699416+tierranaa@users.noreply.github.com>
2024-04-11 14:01:13 -0400

    Update ImageCardHover.js

--- VirtuosoApp/frontend/src/components/carouselcomponents/ImageCardHover.js
+++ VirtuosoApp/frontend/src/components/carouselcomponents/ImageCardHover.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import styles from '../styles/carousel.module.css';
 
-const ImageCardHover = ({ image }) => {
+const ImageCardHover = ({ src, alt }) => {
+    const [isHovering, setIsHovering] = useState(false); 
+
+    const handleMouseEnter = () => {
+        setIsHovering(true);
+    }
+
+    const handleMouseLeave = () => {
+        setIsHovering(false);
+    }
+   
     return (
-        <div className={`${styles.card} ${styles.hover}`}>
-            <img src={image.url} alt={image.alt} />
-            {/* Additional hover state content here */}
+        <div className={styles['image-card-container']}>
+            <div
+                className={styles['image-card']}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+            >
+                <img src={src} alt={alt} />
+                {isHovering && (
+                    <div className={styles.hoverContent}>
+                        <p>Artwork name</p>
+                        <p>My rating: * * * * *</p>
+                        <p>Review likes: 32</p>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
