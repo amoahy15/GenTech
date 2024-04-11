a6305b9ba6ccb0a2649ed0dab1a4b62f068c4686
joiz1 <157747754+joiz1@users.noreply.github.com>
2024-04-11 14:57:09 -0400

    Update annotationModel.py

--- VirtuosoApp/backend/models/annotationModel.py
+++ VirtuosoApp/backend/models/annotationModel.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 class Annotation(Document):
     artwork_id  = StringField(required=True)
-    userID = StringField(required=True)
+    user_id = StringField(required=True)
     message = StringField(required=True)
     x_coordinate = StringField(required=True)
     y_coordinate = StringField(required=True)
