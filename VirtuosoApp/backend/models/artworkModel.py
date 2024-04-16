9d789e3b7f1141e54a048abd98c0fe23177ce07b
joiz1 <157747754+joiz1@users.noreply.github.com>
2024-04-16 12:55:35 -0400

    Update artworkModel.py

--- VirtuosoApp/backend/models/artworkModel.py
+++ VirtuosoApp/backend/models/artworkModel.py
@@ -21,24 +21,6 @@ class Artwork(Document):
     genre = StringField()
     reviews = ListField(ReferenceField(Review))
 
-    def serialize(self):
-        reviews = [review.serialize() for review in self.reviews]
-        return {
-            "artwork_id": self.artwork_id,
-            "title": self.title,
-            "artist_name": self.artist_name,
-            "artist_id": str(self.artist.id),  #Convert artist ID to string
-            "artist_username": str(self.artist.user_name),  #Convert artist username to string
-            "year": self.year,
-            "image_url": self.image_url,
-            "tags": self.tags,
-            "description": self.description,
-            "image_location": self.image_location,
-            "annotations": self.annotations,
-            "average_rating": self.average_rating,
-            "genre": self.genre,
-            "reviews": reviews
-        }
 
     def update_average_rating(self):
         from .reviewModel import Review
