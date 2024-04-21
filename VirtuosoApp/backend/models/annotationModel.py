af02725e0c1b68f83e24ae6e4d1c9484e95b5f6d
joiz1 <157747754+joiz1@users.noreply.github.com>
2024-04-21 16:37:49 -0400

    Update annotationModel.py

--- VirtuosoApp/backend/models/annotationModel.py
+++ VirtuosoApp/backend/models/annotationModel.py
@@ -1,11 +1,14 @@
-from mongoengine import Document, StringField, DateTimeField
+from mongoengine import Document, StringField, DateTimeField, IntField, ListField, ReferenceField
 from datetime import datetime
+from .userModel import User
 
 class Annotation(Document):
     annotation_id = StringField(required=True, unique=True)
-    artwork_id  = StringField(required=True)
+    artwork_id = StringField(required=True)
     user_id = StringField(required=True)
     message = StringField(required=True)
     x_coordinate = StringField(required=True)
     y_coordinate = StringField(required=True)
     createdAt = DateTimeField(default=datetime.now)
+    like_count = IntField(default=0)
+    liked_by = ListField(ReferenceField('User', required=True))
