04adcc5ab9ab289db4c5d6bac9ee8d271e445977
Chloe Bickel <chl.bickel@gmail.com>
2024-04-22 00:33:20 -0400

    responsive home page

--- VirtuosoApp/frontend/src/components/Pages/Home.js
+++ VirtuosoApp/frontend/src/components/Pages/Home.js
@@ -4,85 +4,97 @@ import Button from "../Navigation/Button";
 import Row from "../Navigation/rowScroll";
 import Carousel from "../carouselcomponents/Carousel";
 import Collections from "./Collections";
-
+import React, { useState, useEffect, onClick} from "react";
+import video from '../../assets/videos/lightvid.mp4'
 const Home = () => {
-  const onClick = () => {
-    console.log("click");
-  };
+
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
-    <div className={styles.container}>
-      <div className={styles.content}>
-        <div className={styles.textContainer}>
-          <h2 className={styles.h2}>
-            <span>Learn</span>
-            <span style={{ color: "#990000" }}> Art</span>
-          </h2>
-          <h2 className={styles.h2}>
-            &nbsp;&nbsp;<span>Love</span>
-            <span style={{ color: "#990000" }}> Art</span>
-          </h2>
-          <h2 className={styles.h2}>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>Live</span>
-            <span style={{ color: "#990000" }}> Art</span>
-          </h2>
-          <p
-            className={styles.p}
-            style={{ marginBottom: "30px", maxWidth: "90%" }}
-          >
-          </p>
-          <h1 className={styles.h1}>
-            <span>Virtuos</span>
-            <span style={{ color: "#990000" }}>o is Painting that Picture</span>
-          </h1>
-          <p
-            className={styles.p}
-            style={{ marginBottom: "30px", maxWidth: "70%" }}
-          >
-          </p>
-          <p
-            className={styles.p}
-            style={{ marginBottom: "30px", maxWidth: "50%" }}
-          >
-            VIRTUOS<span style={{ color: "#990000" }}>O</span> brings art
-            connoisseurs, artists, critics, and enjoyers a social platform that
-            allows you to rate art and create galleries that exemplify your
-            taste.
-          </p>
-          <p
-            className={styles.p}
-            style={{ marginBottom: "30px", maxWidth: "50%" }}
-          >
-            Explore a variety of genres and styles. Start building your
-            community today and dive into our diverse catalogue.
-          </p>
-          <div className={styles.btn}>
-            <a href="./register">
-              <Button text="Create an Account" onClick={onClick} />
-            </a>
+    <div>
+      {
+        !isMobile? (
+          <div className={styles.container}>
+            <div className={styles.content}>
+              <div className={styles.textContainer}>
+                <h2 className={styles.h2}>
+                  <span>Learn</span>
+                  <span style={{ color: "#990000" }}> Art</span>
+                </h2>
+                <h2 className={styles.h2}>
+                  &nbsp;&nbsp;<span>Love</span>
+                  <span style={{ color: "#990000" }}> Art</span>
+                </h2>
+                <h2 className={styles.h2}>
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>Live</span>
+                  <span style={{ color: "#990000" }}> Art</span>
+                </h2>
+                <p
+                  className={styles.p}
+                  style={{ marginBottom: "30px", maxWidth: "90%" }}
+                >
+                </p>
+                <h1 className={styles.h1}>
+                  <span>Virtuos</span>
+                  <span style={{ color: "#990000" }}>o is Painting that Picture</span>
+                </h1>
+                <p className={styles.p}
+                  style={{ marginBottom: "30px", maxWidth: "70%" }}>
+                </p>
+
+                <p className={styles.p} style={{ marginBottom: "30px", maxWidth: "50%" }}>
+                  VIRTUOS<span style={{ color: "#990000" }}>O</span> brings art
+                  connoisseurs, artists, critics, and enjoyers a social platform that
+                  allows you to rate art and create galleries that exemplify your
+                  taste.
+                </p>
+
+                <p
+                  className={styles.p}
+                  style={{ marginBottom: "30px", maxWidth: "50%" }}
+                >
+                  Explore a variety of genres and styles. Start building your
+                  community today and dive into our diverse catalogue.
+                </p>
+                <div className={styles.btn}>
+                  <a href="/register">
+                    <Button text="Create an Account" onClick={onClick} />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div style={{ marginBottom: "10vh" }}></div>
           </div>
+        ): (
+          <div>
+            <div className={styles["txt"]}>
+            
+            <div className={styles["textOverlay"]}>
+              WELCOME TO VIRTUOS<span style={{ color: "#990000" }}>O</span>:<br/>
+              <span style={{fontSize: '15px'}}>The social network for art lovers,</span><br/>
+              <span style={{fontSize: '15px'}}>a space for creatives to curate their thoughts,</span><br/>
+              <span style={{fontSize: '15px'}}>scholars to share their opinions, </span><br/>
+              <span style={{fontSize: '15px'}}>and the curious to discover art</span>
+              </div>
+              <div className={styles["btn"]}>
+                  <a style={{textDecoration: 'none', color: 'white'}}href="/register">
+                    SIGN UP
+                  </a>
+                </div>
         </div>
-      </div>
-
-      <div className={styles.background}>
-        <img
-          src={HomeBackground}
-          alt="Home Background"
-          className={styles.backgroundImage}
-        />
-      </div>
-      <div style={{ marginBottom: "25vh" }}></div>
-
-      <div >
-        <Row title="Paintings">
-          <div style={{paddingBottom: '50px', padding: '10px 8vw'}}>
-            <Collections category="painting" />
           </div>
-          
-        </Row>
-        
-        <img></img>
-      </div>
+
+        )
+      }
     </div>
   );
 };
