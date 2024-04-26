1c366126350c9e775c0e86f606b449a5048dd4ce
Tierra Ablorh <122699416+tierranaa@users.noreply.github.com>
2024-04-26 12:55:48 -0400

    Update App.js

--- VirtuosoApp/frontend/src/App.js
+++ VirtuosoApp/frontend/src/App.js
@@ -21,6 +21,9 @@ import Loading from "./components/Navigation/Loading";
 import Search from "./components/API/Search";
 import AdvancedSettings from "./components/UserData/AdvancedSettings";
 import OtherUser from "./components/Pages/OtherUsers";
+import { NotificationProvider } from './components/Notifications/notificationProvider.js';
+import NotificationBar from './components/Notifications/notificationBar.js';
+
 
 function App() {
   const token = localStorage.getItem("token");
@@ -34,80 +37,84 @@ function App() {
   return (
     <Router>
       <div className="App">
-        {loading && <Loading />}
-        <div className="Content">
-          <Switch>
-            <Route exact path="/">
-              {token ? <NavUser /> : <Nav />}
-              <Home />
-              <Footer />
-            </Route>
-            <Route path="/about">
-              {token ? <NavUser /> : <Nav />}
-              <AboutPage />
-              <Footer />
-            </Route>
-            <Route
-              path="/reviews/:artworkID"
-              render={(props) => (
-                <div>
-                  {token ? <NavUser /> : <Nav />}
-                  <ReviewPage {...props} />
-                  <Footer />
-                </div>
-              )}
-            />
-            <Route path="/login">
-              <LoginPage />
-            </Route>
-            <Route path="/register">
-              <Register />
-            </Route>
-            <Route path="/profile">
-              {token ? <NavUser /> : <Nav />}
-              <Profile />
-              <Footer />
-            </Route>
-            <Route path="/collections">
-              {token ? <NavUser /> : <Nav />}
-              <Gallery />
-              <Footer />
-            </Route>
-            <Route path="/disclaimer">
-              <Disclaimer />
-            </Route>
-            <Route path="/test">
-              <TestSearch />
-            </Route>
-            <Route path="/reset-password">
-              <ResetPassword />
-            </Route>
-            <Route path="/reset-password-check">
-              <ResetPasswordCheck />
-            </Route>
-            <Route path="/email-check">
-              <EmailCheckPage />
-            </Route>
-            <Route path="/search">
-            {token ? <NavUser /> : <Nav />}
-              <Search />
-              <Footer></Footer>
-            </Route>
-            <Route
-              path="/verify/:userId/:verificationToken"
-              component={VerificationPage}
-            />
-            <Route
-              path="/reset_password/:resetToken"
-              component={ResetPasswordPage}
-            />
-            <Route path="/settings">
+        <NotificationProvider> 
+          <NotificationBar />
+          {loading && <Loading />}
+          <div className="Content">
+            <Switch>
+              <Route exact path="/">
+                {token ? <NavUser /> : <Nav />}
+                <Home />
+                <Footer />
+              </Route>
+              <Route path="/about">
+                {token ? <NavUser /> : <Nav />}
+                <AboutPage />
+                <Footer />
+              </Route>
+              <Route
+                path="/reviews/:artworkID"
+                render={(props) => (
+                  <div>
+                    {token ? <NavUser /> : <Nav />}
+                    <ReviewPage {...props} />
+                    <Footer />
+                  </div>
+                )}
+              />
+              <Route path="/login">
+                <LoginPage />
+              </Route>
+              <Route path="/login2">
+                <LoginPage />
+              </Route>
+              <Route path="/register">
+                <Register />
+              </Route>
+              <Route path="/profile">
+                {token ? <NavUser /> : <Nav />}
+                <Profile />
+                <Footer />
+              </Route>
+              <Route path="/collections">
+                {token ? <NavUser /> : <Nav />}
+                <Gallery />
+                <Footer />
+              </Route>
+              <Route path="/disclaimer">
+                <Disclaimer />
+              </Route>
+              <Route path="/test">
+                <TestSearch />
+              </Route>
+              <Route path="/reset-password">
+                <ResetPassword />
+              </Route>
+              <Route path="/reset-password-check">
+                <ResetPasswordCheck />
+              </Route>
+              <Route path="/email-check">
+                <EmailCheckPage />
+              </Route>
+              <Route path="/search">
+                <Search />
+              </Route>
+              <Route
+                path="/verify/:userId/:verificationToken"
+                component={VerificationPage}
+              />
+              <Route
+                path="/reset_password/:resetToken"
+                component={ResetPasswordPage}
+              />
+               <Route path="/settings">
             {token ? <NavUser /> : <Nav />}
               <AdvancedSettings/>
             </Route>
             <Route path="/profiles/:username" component={OtherUser} />
-          </Switch>
-        </div>
+            </Switch>
+          </div>
+        </NotificationProvider>
       </div>
     </Router>
   );
