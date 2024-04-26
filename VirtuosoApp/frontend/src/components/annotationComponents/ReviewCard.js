8d992545bf6a03daa71ef2b07fa91a3dfebe33c5
Chloe Bickel <chl.bickel@gmail.com>
2024-04-25 22:44:15 -0400

    stylistic changes

--- VirtuosoApp/frontend/src/components/annotationComponents/ReviewCard.js
+++ VirtuosoApp/frontend/src/components/annotationComponents/ReviewCard.js
@@ -3,13 +3,13 @@ import styles from '../styles/reviews.module.css';
 import StaticStarRating from './staticstars';
 import { FaTrashAlt, FaHeart} from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-
+import linkstyle from '../styles/annotation.module.css'
 //This component displays a single review card & controls conditional components
 const ReviewCard = ({ rating, likes, user, review, revid, is_owner, onDelete, onLike, isLiked}) => {
   return (
     <div className={styles['review-card']}>
       <StaticStarRating rating={rating}/>
-      <Link to={`/profiles/${user}`}><p style={{fontSize: '20px', paddingBottom: '5px'}}>{user}</p></Link>
+      <Link className={linkstyle['link']} to={`/profiles/${user}`}><p style={{fontSize: '20px', paddingBottom: '5px'}}>{user}</p></Link>
       <p style={{overflow: 'default'}}>{review}</p>
       <div className={styles['actions']}>
         <button onClick={() => onLike(revid)} className={styles['like-button']}>
